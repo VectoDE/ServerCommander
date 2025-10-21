917d914a6ba82e6b1656ca42eedd72e54d349ab3
VectoDE <system.bot.discord.utility@gmail.com>
2025-10-21 19:22:56 +0200

    Create desktop console window with context menu

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module servercommander
 
 go 1.23.5
+
+require fyne.io/fyne/v2 v2.4.5
--- src/main.go
+++ src/main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"log"
 
 	"servercommander/src/cmd"
 	"servercommander/src/ui"
-	"servercommander/src/utils"
 )
 
 func main() {
-	ui.ApplicationBanner()
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		fmt.Print(utils.Yellow, "\n>> ", utils.Reset)
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(utils.Red, "Failed to read input:", err, utils.Reset)
-			continue
-		}
-
-		command := strings.TrimSpace(input)
-		if command == "" {
-			continue
-		}
-
-		if err := cmd.Execute(command); err != nil {
-			fmt.Println(utils.Red, err, utils.Reset)
-		}
+	if err := ui.RunStandaloneConsole(cmd.Execute); err != nil {
+		log.Fatal(err)
 	}
 }
